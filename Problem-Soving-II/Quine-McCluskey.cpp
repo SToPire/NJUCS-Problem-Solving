b3c41afe9423fea6860b526ed9a6dd5c1ebb94f8
SToPire <46126592+SToPire@users.noreply.github.com>
2019-03-13 15:10:34 +0800

    Update Quine-McCluskey.cpp

--- Problem-Soving-II/Quine-McCluskey.cpp
+++ Problem-Soving-II/Quine-McCluskey.cpp
@@ -5,15 +5,16 @@
 #include<bitset>
 #include<map>
 using namespace std;
+int nvariable;
 struct implicant {
-	bitset<32> num; //蕴含项的二进制表示
-	bitset<32> mask; //被用斜杠代替的数
+	bitset<8> num; //蕴含项的二进制表示
+	bitset<8> mask; //被用斜杠代替的数
 	bool choose; //是否是质蕴涵项
 	set<int> si;//该蕴含项所代表的整数集合
 
 	implicant() = default;
 	implicant(int n) :num(n), mask(0), choose(true) { si.insert(n); }
-	implicant(bitset<32> a, bitset<32> b, bool c, set<int> s) :num(a), mask(b), choose(c) { si.insert(s.begin(), s.end()); }
+	implicant(bitset<8> a, bitset<8> b, bool c, set<int> s) :num(a), mask(b), choose(c) { si.insert(s.begin(), s.end()); }
 
 	bool operator==(const implicant& rhs) 
 	{
@@ -32,8 +33,14 @@ ostream& operator<<(ostream& os, implicant a)
 			os << (int)a.num[i];
 		--i;
 	}
-	os << "     set:";
+	os << "  set:";
 	for (auto i : a.si) os << i << ' ';
+	os << "  对应项:";
+	for (int k=0,j=nvariable-1; j != -1; --j,++k) {
+		if (a.mask[j] == 1) continue;
+		if (a.num[j] == 1) os << static_cast<char>('A' + k);
+		if (a.num[j] == 0)os << static_cast<char>('A' + k) << '\'';
+	}
 	return os;
 }
 bool findSubset(set<int>& l, set<int>& r) //判断l是否为r的真子集
@@ -43,6 +50,17 @@ bool findSubset(set<int>& l, set<int>& r) //判断l是否为r的真子集
 		if (r.find(a) != r.end()) ++count;
 	}
 	return count==l.size() && l.size()<r.size();
+}
+bool equalSet(set<int>& a, set<int>& b)
+{
+	bool flag = true;
+	for (auto n : a) {
+		if (b.find(n) == b.end()) flag = false;
+	}
+	for (auto n : b) {
+		if (a.find(n) == a.end()) flag = false;
+	}
+	return flag;
 }
 template<typename T>
 set<T>& setMinus(set<T>& a, set<T>& b) //计算集合a减集合b,返回a的引用
@@ -73,19 +91,22 @@ void QuineMcCluskey(vector<implicant>& v, set<int> s)
 			}
 		}
 	for (auto &a : v) {
-		if (a.choose) continue;
-		setMinus(a.si, selected);
-	}
-	vector<implicant> v2;
-	for (auto a : v) {
 		if (a.choose) {
 			res.push_back(a);
 			continue;
 		}
+		setMinus(a.si, selected);
+	}
+	vector<implicant> v2;
+	for (auto a : v) {
+		if (a.choose) continue;
 		bool flag = true;
 		for (auto b : v) {
 			if (b.choose) continue;
 			if (findSubset(a.si, b.si)) flag = false;
+		}
+		for (auto c : v2) {
+			if (equalSet(a.si,c.si)) flag = false;
 		}
 		if (flag) v2.push_back(a);
 	}
@@ -94,17 +115,21 @@ void QuineMcCluskey(vector<implicant>& v, set<int> s)
 }
 int main(void)
 {
+	cout << "请输入逻辑函数变量个数:";
+	cin >> nvariable;
 	int n,temp;
+	cout << "请输入最小项个数:";
 	cin >> n;
 	set<int> input;        
 	vector<list<implicant>> vec(33);   //vec[i]是二进制中有i个1的蕴涵式构成的list
+	cout << "请输入最小项编号:";
 	for (int i = 1; i <= n; ++i) {
 		cin >> temp;
 		input.insert(temp);
-		vec[bitset<33>(temp).count()].push_back(implicant(temp));
+		vec[bitset<9>(temp).count()].push_back(implicant(temp));
 	}
 
-	for (int i = 0; i <= 31; ++i) {
+	for (int i = 0; i <= 7; ++i) {
 		for (auto &a : vec[i]) {
 			if (vec[i + 1].empty()) break;   //处理的范围是有数据的list，最后一个除外，因为每一组要跟下一组作比较
 			for (auto &b : vec[i + 1]) {
